b8ee2734b2b1e4c10ec8f547973fc257f37cb93e
Imoh Henry <henryimoh2@gmail.com>
2023-12-21 10:21:16 +0100

    Add files via upload

--- js/download-pdf.js
+++ js/download-pdf.js
@@ -7,19 +7,18 @@ downloadPdf.addEventListener("click", () => {
     // Creating the element anchor that 
     // will download pdf 
     let element = document.createElement("a"); 
-    element.href = "../QTB-eBook.pdf"; 
-    element.download = "../QTB-eBook.pdf"; 
-
-    // Adding the element to body 
-    document.documentElement.appendChild(element); 
-
-    // Above code is equivalent to 
-    // <a href="path of file" download="file name">  
-
-    // onClick property, to trigger download 
-    element.click(); 
-
-    // Removing the element from body 
-    document.documentElement.removeChild(element); 
+    element.href = "https://mcusercontent.com/a3d687905d2a2c4b266d653e8/files/8b0ca48d-67fa-db1f-46de-a072b30ef336/QTB_eBook.pdf"; 
+    element.download = "QTB_eBook.pdf";
+    
+// Opening the link in a new tab after a delay of 4 seconds
+setTimeout(() => {
+    element.target = "_blank";
+    // Adding the element to body
+    document.documentElement.appendChild(element);
+    // onClick property, to trigger download
+    element.click();
+    // Removing the element from body
+    document.documentElement.removeChild(element);
+}, 2000); // 4000 milliseconds = 4 seconds
 
 })
